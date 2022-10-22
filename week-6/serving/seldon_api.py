dae7c705216addfe71a08b239f9dd2516d6ed0bd
truskovskiyk <truskovskiyk@gmail.com>
2022-10-22 13:44:25 -0400

    update

--- week-6/serving/seldon_api.py
+++ week-6/serving/seldon_api.py
@@ -7,18 +7,18 @@ import numpy as np
 
 logger = logging.getLogger()
 
-class Score:
-    def __init__(self, tp, fp, tn, fn):
-        self.tp = tp
-        self.fp = fp
-        self.tn = tn
-        self.fn = fn
+# class Score:
+#     def __init__(self, tp, fp, tn, fn):
+#         self.tp = tp
+#         self.fp = fp
+#         self.tn = tn
+#         self.fn = fn
         
 class SeldonAPI:
     def __init__(self, model_id: Optional[str] = None):
         self.predictor = Predictor.default_from_model_registry(model_id=model_id)
-        self.scores = Score(tp=0, fp=0, tn=0, fn=0)
         self._run_time = None
+        # self.scores = Score(tp=0, fp=0, tn=0, fn=0)
 
     def predict(self, text, features_names: List[str]):
         logger.info(text)
@@ -32,36 +32,36 @@ class SeldonAPI:
         return results
 
 
-    def send_feedback(self, features, feature_names, reward, truth, routing=""):
-        logger.info("features")
-        logger.info(features)
+    # def send_feedback(self, features, feature_names, reward, truth, routing=""):
+    #     logger.info("features")
+    #     logger.info(features)
         
-        logger.info("truth")
-        logger.info(truth)
+    #     logger.info("truth")
+    #     logger.info(truth)
 
-        results = self.predict(features, feature_names)
-        predicted = np.argmax(results, axis=1)
+    #     results = self.predict(features, feature_names)
+    #     predicted = np.argmax(results, axis=1)
 
 
-        if int(truth[0]) == 1:
-            if int(predicted[0]) == int(truth[0]):
-                self.scores.tp += 1
-            else:
-                self.scores.fn += 1
-        else:
-            if int(predicted[0]) == int(truth[0]):
-                self.scores.tn += 1
-            else:
-                self.scores.fp += 1
+    #     if int(truth[0]) == 1:
+    #         if int(predicted[0]) == int(truth[0]):
+    #             self.scores.tp += 1
+    #         else:
+    #             self.scores.fn += 1
+    #     else:
+    #         if int(predicted[0]) == int(truth[0]):
+    #             self.scores.tn += 1
+    #         else:
+    #             self.scores.fp += 1
 
-        return []
+    #     return []
 
     def metrics(self):
 
         return [
             {"type": "GAUGE", "key": "gauge_runtime", "value": self._run_time},
-            {"type": "GAUGE", "key": f"true_pos", "value": self.scores.tp},
-            {"type": "GAUGE", "key": f"true_neg", "value": self.scores.fn},
-            {"type": "GAUGE", "key": f"false_pos", "value": self.scores.fn},
-            {"type": "GAUGE", "key": f"false_neg", "value": self.scores.fp},
+            # {"type": "GAUGE", "key": f"true_pos", "value": self.scores.tp},
+            # {"type": "GAUGE", "key": f"true_neg", "value": self.scores.fn},
+            # {"type": "GAUGE", "key": f"false_pos", "value": self.scores.fn},
+            # {"type": "GAUGE", "key": f"false_neg", "value": self.scores.fp},
         ]        
